d5e75bd0fe55c2b74996b915e731d01c10887a5b
akashdeep931 <akashdeepsingh931@gmail.com>
2023-07-20 13:43:42 +0100

    setup sql for testing and development environment

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg");
 
-require("dotenv").config();
+const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
 if (!process.env.PGDATABASE) {
   throw Error("PGDATABASE not set");
