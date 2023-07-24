e1564d18f5fde1b41054ff53317c0dbd1649119a
akashdeep931 <akashdeepsingh931@gmail.com>
2023-07-24 11:38:10 +0100

    set-up instance of the database online and start implementing the scripts for hosting and seeding the online database

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,16 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 3,
+      }
+    : {};
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
