10b692bdc01a84b4a47c8d55759478616d13791f
akashdeep931 <akashdeepsingh931@gmail.com>
2023-07-24 14:21:22 +0100

    use firebase-session-storage to store the sessions

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const session = require("express-session");
+const FirebaseStore = require("connect-session-firebase")(session);
+const firebase = require("firebase-admin");
 const passport = require("passport");
 const authRouter = require("./routes/authentication-routes");
 const apiRouter = require("./routes/api-routes");
@@ -14,11 +16,19 @@ require("dotenv").config({
   path: `${__dirname}/.env.production`,
 });
 
+const ref = firebase.initializeApp({
+  credential: firebase.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
 app.use(
   session({
     secret: process.env.SECRET_STRING,
     resave: false,
     saveUninitialized: true,
+    store: new FirebaseStore({
+      database: ref.database()
+    }),
     cookie: {
       maxAge: 1000 * 60 * 60 * 78,
     },
