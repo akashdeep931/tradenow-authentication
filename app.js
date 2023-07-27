fad259593cc4800a3854fa33355b79b4da525e66
akashdeep931 <akashdeepsingh931@gmail.com>
2023-07-27 17:20:06 +0100

    try new cors origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://localhost:5173",
+    origin: "https://localhost:5173/login",
     credentials: true,
   })
 );
