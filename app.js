80420945a66ac1e10e7be8f4e38971c4c8dfef88
akashdeep931 <akashdeepsingh931@gmail.com>
2023-07-27 15:02:11 +0100

    correct origin

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173/login", "http://localhost:5173/register"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   })
