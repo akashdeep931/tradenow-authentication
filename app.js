355beeedb0768f82844231cd278679689b0fa935
akashdeep931 <akashdeepsingh931@gmail.com>
2023-07-28 17:07:54 +0100

    change origin

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://64c3e796035c7b0fef2f7658--tdnow.netlify.app/",
     credentials: true,
   })
 );
