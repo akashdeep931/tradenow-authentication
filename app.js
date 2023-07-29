5ad07675cfa520097fc0d307722d06bfac8826aa
akashdeep931 <akashdeepsingh931@gmail.com>
2023-07-29 12:37:07 +0100

    change httpOnly to false

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(
     cookie: {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
+      httpOnly: false,
       maxAge: 1000 * 60 * 60 * 78,
     },
   })
